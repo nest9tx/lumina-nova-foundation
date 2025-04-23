20bcf18d47b6c5673fb2f65ec80d7693c1f079c0
nest9tx <nest9@yahoo.com>
2025-04-23 09:12:21 -0500

    Restore working directory structure for Vercel

--- src/utils/supabase/server.ts
+++
@@ -1,8 +0,0 @@
-// src/utils/supabase/server.ts
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
-import { cookies } from 'next/headers';
-
-export function createClient() {
-  return createServerComponentClient({ cookies });
-}
-
