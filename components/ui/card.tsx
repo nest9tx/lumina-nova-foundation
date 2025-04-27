235ecbc242b605884e4131007424d5f518e0454a
nest9tx <nest9@yahoo.com>
2025-04-27 17:38:33 -0500

    Update: awaken cards and button 1

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -1,19 +1,20 @@
+"use client";
+
 import * as React from "react";
+import { cn } from "@/lib/utils";
 
-import { cn } from "@/utils/cn";
-
-export interface CardProps extends React.HTMLAttributes<HTMLDivElement> {}
-
-const Card = React.forwardRef<HTMLDivElement, CardProps>(({ className, ...props }, ref) => (
-  <div
-    ref={ref}
-    className={cn(
-      "rounded-xl border bg-white/10 backdrop-blur-md shadow-md p-6 transition hover:shadow-lg",
-      className
-    )}
-    {...props}
-  />
-));
+const Card = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
+  ({ className, ...props }, ref) => (
+    <div
+      ref={ref}
+      className={cn(
+        "rounded-lg border bg-card text-card-foreground shadow",
+        className
+      )}
+      {...props}
+    />
+  )
+);
 Card.displayName = "Card";
 
 export { Card };
