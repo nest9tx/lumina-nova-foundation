a45d0835c261b36ccf9f99477d039881e81adc4f
“nest9tx” <nest9@yahoo.com>
2025-05-01 05:44:24 -0500

    🧽 cleanse: delete legacy dynamic routes and start clean under /vaults/

--- app/living-scrolls/vaults/[vaultName]/[scrollName]/page.tsx
+++ app/living-scrolls/vaults/[vaultName]/[scrollName]/page.tsx
@@ -4,21 +4,19 @@ import { Box, Heading } from '@chakra-ui/react'
 
 interface PageProps {
   params: {
-    vault: string;
-    scroll: string;
+    vaultName: string;
+    scrollName: string;
   };
 }
 
 export default function Page(props: PageProps) {
-  const { vault, scroll } = props.params
+  const { vaultName, scrollName } = props.params
 
   return (
     <Box p={8}>
       <Heading size="xl">
-        Scroll: {scroll} from Vault: {vault}
+        Scroll: {scrollName} from Vault: {vaultName}
       </Heading>
     </Box>
   )
 }
-
-
