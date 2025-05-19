fd1d8200a6debcf480b7a9dc216113662c5001cb
“nest9tx” <nest9@yahoo.com>
2025-05-19 10:04:45 -0500

    🌀 updated google-track component🌀

---
+++ components/google-tag.tsx
@@ -0,0 +1,25 @@
+// components/google-tag.tsx
+import Script from "next/script";
+
+export default function GoogleTag() {
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=AW-17077275471"
+        strategy="afterInteractive"
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-17077275471');
+          `,
+        }}
+      />
+    </>
+  );
+}
